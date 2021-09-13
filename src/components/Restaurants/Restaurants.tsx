5ae386086210d7da2345c32ae2b7c48e8b723577
parth-007 <parthmangukiya77@gmail.com>
2021-09-13 21:15:48 +0530

    API call to get data

--- src/components/Restaurants/Restaurants.tsx
+++ src/components/Restaurants/Restaurants.tsx
@@ -1,4 +1,21 @@
+import {useState, useEffect} from 'react';
+
 const Restaurants = () => {
+
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(()=>{
+        const url = 'https://my-restaurants-api.herokuapp.com/restaurants';
+        const fetchRestaurants = async() => {
+            await fetch(url).
+                then(res => res.json()).
+                then(data => setRestaurants(data)).
+                catch(e => console.error(e));
+        }
+
+        fetchRestaurants();
+    },[]);
+
     return (
         <div>Restaurants Data here!</div>
     );
